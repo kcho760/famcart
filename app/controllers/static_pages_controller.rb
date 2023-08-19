64c85071c9fb4b846415aaec7944737680662329
kcho760 <kcho760@gmail.com>
2023-08-18 20:29:37 -0400

    Render setup

--- app/controllers/static_pages_controller.rb
+++
@@ -1,9 +0,0 @@
-# app/controllers/static_pages_controller.rb
-
-class StaticPagesController < ActionController::Base
-    def frontend_index
-      render file: Rails.root.join('public', 'index.html')
-    end
-
-    get '*path', to: "static_pages#frontend_index"
-  end
