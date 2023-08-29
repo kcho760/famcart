a46c0eea45f7092701f26fc42f301bbefea860e5
kcho760 <kcho760@gmail.com>
2023-08-28 20:21:16 -0400

    Put nested list_items back in

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
 
-  resources :lists, only: [:create, :update, :destroy, :index, :show]
+  resources :lists do
+    resources :list_items, only: [:create, :update]
+  end
 
   resources :items, only: [:create, :update, :destroy, :index, :show]
   resources :list_items, only: [:create] # Add the create action for list_items
