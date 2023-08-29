1583d548f875acf61a19a1d7ba44d8a01c634d38
kcho760 <kcho760@gmail.com>
2023-08-28 23:11:24 -0400

    Moved drop

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,11 +3,11 @@ set -o errexit
 set -o nounset
 
 # render-deploy.sh
-rails db:drop
 
 # Perform any necessary build steps (e.g., npm run build)
 bundle install
 
+rails db:drop
 # Run database migrations as part of the deployment process
 rails db:migrate
 
