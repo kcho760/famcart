1e600f206ac69a9d1035a6933317832655cd9df6
kcho760 <kcho760@gmail.com>
2023-11-09 21:26:26 -0500

    loggers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::API
         include DeviseTokenAuth::Concerns::SetUserByToken
-end
+      
+        before_action :log_request_data
+      
+        private
+      
+        def log_request_data
+          Rails.logger.info "Headers: #{request.headers.inspect}"
+          Rails.logger.info "Body: #{request.body.read}"
+        end
+      end
